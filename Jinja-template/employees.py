9506cf607f9268c72fae3f31af4f2f5a3816a453
Aditya vardhan <75056596+adityavardhanshakya@users.noreply.github.com>
2024-06-20 10:54:03 +0530

    Add files via upload

---
+++ Jinja-template/employees.py
@@ -0,0 +1,28 @@
+# dummy database
+employees_data = {
+    1: {
+        "name": "Aditya Vardhan Shakya",
+        "age": 26,
+        "position": "Manager"
+    },
+    2: {
+        "name": "Sheelvardhan",
+        "age": 35,
+        "position": "Salesperson"
+    },
+    3: {
+        "name": "Pam",
+        "age": 42,
+        "position": "Salesperson"
+    },
+    4: {
+        "name": "Jim",
+        "age": 31,
+        "position": "Salesperson"
+    },
+    5: {
+        "name": "Josh",
+        "age": 40,
+        "position": "Manager"
+    }
+}
